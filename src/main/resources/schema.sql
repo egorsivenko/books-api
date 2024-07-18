263730634f0923917834742e839936ac7124741d
EgorSivenko <egor.siv20@gmail.com>
2024-07-18 12:03:28 +0300

    Added db schema initialization

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS authors;
+DROP TABLE IF EXISTS books;
+
+CREATE TABLE authors
+(
+    id   BIGSERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    age  INT  NOT NULL
+);
+
+CREATE TABLE books
+(
+    isbn      TEXT PRIMARY KEY NOT NULL,
+    title     TEXT             NOT NULL,
+    author_id BIGINT           NOT NULL,
+    FOREIGN KEY (author_id) REFERENCES authors (id)
+);
